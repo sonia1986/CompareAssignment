e0a3dcf898baca088c6259906b5559ab6829015e
sonia1986 <soniasaini86@gmail.com>
2019-07-12 21:20:44 +0530

    Added first test cases.

--- src/test/java/CompareTestcase.java
+++ src/test/java/CompareTestcase.java
@@ -6,10 +6,15 @@ import reader.FileReader;
 public class CompareTestcase {
 
     @Test
-    public void test_is_file_url_count_equal_and_match_with_avaliable_data(){
+    public void test_is_file_url_count_equal_and_match_with_avaliable_data() throws Exception{
         FileReader.start();
-        int countUrlFromFile1 = FileInfo.getSingleton().getLineCount1();
-        int countUrlFromFile2 = FileInfo.getSingleton().getLineCount2();
-        Assert.assertEquals(countUrlFromFile1,countUrlFromFile2, "Url count is matched.");
+        int countUrlForFile1 = FileInfo.getSingleton().getLineCount1();
+        int countUrlForFile2 = FileInfo.getSingleton().getLineCount2();
+        System.out.println("Counts are:: " + countUrlForFile1 + "Another file count::" + countUrlForFile2);
+        if (countUrlForFile1 != countUrlForFile2) {
+            Assert.fail("Any of the file has less numbers of url so only given url will be matched.");
+        } else {
+            Assert.assertEquals(countUrlForFile1,countUrlForFile2);
+        }
     }
 }
