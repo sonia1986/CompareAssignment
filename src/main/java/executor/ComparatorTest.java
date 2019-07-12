3a90c3a51bcddec86875591ce6df5894df7d95ba
sonia1986 <soniasaini86@gmail.com>
2019-07-12 17:21:42 +0530

    Update ComparatorTest.java

--- src/main/java/executor/ComparatorTest.java
+++ src/main/java/executor/ComparatorTest.java
@@ -9,19 +9,22 @@ import java.util.concurrent.Executors;
 
 public class ComparatorTest {
     public static void main(String[] args){
+        try {
         FileReader.start();
-        System.out.println("Server is started.");
+        System.out.println("Info : Server is started.");
 
         FileInfo  fileInfo = FileInfo.getSingleton();
-        System.out.println("Have started compare process.");
+        System.out.println("Info : Have started compare process.");
 
         ExecutorService  executorService = Executors.newFixedThreadPool(100);
-        for(int i = 0 ; i<fileInfo.getMinLength(); i++){
+        for(int i = 0 ; i<fileInfo.getMinLength(); i++) {
             Task task = new Task(i , fileInfo.getUrlArray1()[i], fileInfo.getUrlArray2()[i]);
             executorService.submit(new ProcessTask(task));
-
         }
         executorService.shutdown();
+        } catch (Exception e) {
+            System.out.println("Exception : "+ e.getMessage());
+        }   
 
     }
 }
