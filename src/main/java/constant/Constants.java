550d33675538b315d305eb04267779bdfddedae0
sonia1986 <soniasaini86@gmail.com>
2019-07-12 17:17:02 +0530

    Refactored constant.

--- src/main/java/constant/Constants.java
+++ src/main/java/constant/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     public static final String PROPERTIES_NAME = "fileName.properties";
     public static final String FILE_NAME1      = "fileName1";
     public static final String FILE_NAME2      = "fileName2";
+    public static final String JSON = "JSON";
+    public static final String XML = "XML";
+    public static final String ALL = "ALL";
 }
