fc08524d8f1e40d2d05792d714c76f6ffe3a9305
sonia1986 <soniasaini86@gmail.com>
2019-07-12 17:54:00 +0530

    Modified JSONComparator

--- src/main/java/impl/JSONComparator.java
+++ src/main/java/impl/JSONComparator.java
@@ -6,18 +6,6 @@ public class JSONComparator implements IComparator {
 
     @Override
     public boolean compare(Object obj1, Object obj2){
-        if(obj1 == null && obj2 == null)
-            System.out.println("Response is empty");
-
-         if(obj1 instanceof JSONObject && obj2 instanceof JSONObject){
-             System.out.println("Compare of JSON");
-
-             return true;
-
-         } else {
-             System.out.println("Response is not in JSON. hence going to check XML.");
-         }
-
-         return  false;
+        return  false;
     }
 }
