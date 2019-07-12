e0cbc2057133e92c9a64299f09549e365e5802a5
sonia1986 <soniasaini86@gmail.com>
2019-07-12 17:54:46 +0530

    Modified XMLComparator

--- src/main/java/impl/XMLComparator.java
+++ src/main/java/impl/XMLComparator.java
@@ -4,9 +4,6 @@ public class XMLComparator implements IComparator {
 
     @Override
     public boolean compare(Object obj1, Object obj2){
-
-        System.out.println("Doing XML");
         return false;
-
     }
 }
