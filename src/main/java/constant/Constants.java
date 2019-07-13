b34b459dfb40aef12b3fb1611d67979e1ee616a2
sonia1986 <soniasaini86@gmail.com>
2019-07-13 16:29:49 +0530

    Updated constants.java file.

--- src/main/java/constant/Constants.java
+++ src/main/java/constant/Constants.java
@@ -4,7 +4,11 @@ public class Constants {
     public static final String PROPERTIES_NAME = "fileName.properties";
     public static final String FILE_NAME1      = "fileName1";
     public static final String FILE_NAME2      = "fileName2";
+    
     public static final String JSON = "JSON";
     public static final String XML = "XML";
     public static final String ALL = "ALL";
+    
+    public static final String EQUALS = "equals";
+    public static final String NOT_EQUALS = "not equals";
 }
