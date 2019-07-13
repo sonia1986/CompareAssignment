c23d59b989b32e463a4fb2e947e82ff1d2b406ae
sonia1986 <soniasaini86@gmail.com>
2019-07-13 16:34:36 +0530

    Used Runnable to avoid random exceptions for url.

--- src/main/java/thread/ProcessTask.java
+++ src/main/java/thread/ProcessTask.java
@@ -2,27 +2,25 @@ package thread;
 
 import bean.Task;
 import client.RestClient;
+import static constant.Constants.*;
 import impl.ComparatorFactory;
 import impl.IComparator;
 
-import java.util.concurrent.Callable;
-
-public class ProcessTask implements Callable {
+public class ProcessTask implements Runnable {
 
     private Task task;
 
     public ProcessTask(Task task){this.task = task;}
 
     @Override
-    public Object call(){
+    public void run() {
         process(task);
-        return null;
-    }
+     }
 
     public  void  process(Task task){
         IComparator comparator = ComparatorFactory.getComparator("ALL");
         boolean flag =  comparator.compare(new RestClient().invokeRequest(task.getUrl1()), new RestClient().invokeRequest(task.getUrl2()));
-        String message = flag ? "equals" : "not equals";
+        String message = flag ? EQUALS : NOT_EQUALS;
         message = task.getIndex() + " - " +task.getUrl1() + " " + message + " " + task.getUrl2();
         System.out.println(message);
 
