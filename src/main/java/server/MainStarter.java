968a4b57d3d3c3dced551c729697de1c39b468c2
sonia.saini <soniasaini86@gmail.com>
2019-07-14 21:49:04 +0530

    Refactored some libraries and added more data set variations.

--- src/main/java/server/MainStarter.java
+++ src/main/java/server/MainStarter.java
@@ -3,10 +3,10 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableAutoConfiguration
-public class MainStarter {
+public class MainStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MainStarter.class, args);
     }
